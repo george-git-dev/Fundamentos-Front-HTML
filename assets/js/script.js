66d0301cefae14f2023030c536c4c64debd51352
George <george_roots@hotmail.com>
2021-10-22 18:55:57 -0300

    [Feature George - Task: ADD Js]

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/*
+let nome = window.document.getElementsById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+*/
+
+
+nome.style.width = "100%"
+email.style.width = "100%"
+
+
+
+function validaNome() {
+    if (nome.value.lenght < 3) {
+        alert("nome invalido")
+    }
+
+
+}
+
+document.write("TESTEEEEEEEEEEEEEEEEE")
